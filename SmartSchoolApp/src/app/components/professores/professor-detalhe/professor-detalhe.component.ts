a4e92a03cfde93630ce0dc78b8d2ca4fabb52a63
Gustavo Vieira <gustavovs0241@gmail.com>
2025-03-31 13:31:31 -0300

    implementação professores

--- SmartSchoolApp/src/app/components/professores/professor-detalhe/professor-detalhe.component.ts
+++ SmartSchoolApp/src/app/components/professores/professor-detalhe/professor-detalhe.component.ts
@@ -1,16 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Professor } from '../../../models/Professor';
+import { Subject, takeUntil } from 'rxjs';
+import { Aluno } from '../../../models/Aluno';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { AlunoService } from '../../../services/aluno.service';
+import { ProfessorService } from '../../../services/professor.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-professor-detalhe',
   templateUrl: './professor-detalhe.component.html',
   styleUrls: ['./professor-detalhe.component.css'],
-  standalone: false
+  standalone: false,
 })
 export class ProfessorDetalheComponent implements OnInit {
+  public modalRef: BsModalRef;
+  public professorSelecionado: Professor;
+  public titulo = '';
+  public alunosProfs: Aluno[];
+  private unsubscriber = new Subject<void>();
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private professorService: ProfessorService,
+    private alunoService: AlunoService,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) {}
 
-  ngOnInit() {
+  openModal(template: TemplateRef<any>, alunoId: number) {
+    this.alunosProfessores(template, alunoId);
   }
 
+  closeModal() {
+    this.modalRef.hide();
+  }
+
+  alunosProfessores(template: TemplateRef<any>, id: number) {
+    this.spinner.show();
+    this.alunoService
+      .getByDisciplinaId(id)
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe({
+        next: (alunos: Aluno[]) => {
+          this.alunosProfs = alunos;
+          this.modalRef = this.modalService.show(template);
+        },
+        error: (error: any) => {
+          this.toastr.error(`erro: ${error}`);
+          console.log(error);
+        },
+        complete: () => this.spinner.hide()
+      });
+  }
+
+  ngOnInit() {
+    this.spinner.show();
+    this.carregarProfessor();
+  }
+
+  carregarProfessor() {
+    const profId = +(this.route.snapshot.paramMap.get('id') ?? '00');
+    this.professorService
+      .getById(profId)
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe({
+        next: (professor: Professor) => {
+          this.professorSelecionado = professor;
+          this.titulo = 'Professor: ' + this.professorSelecionado.id;
+          this.toastr.success('Professor carregado com Sucesso!');
+        },
+        error: (error: any) => {
+          this.toastr.error('Professor não carregados!');
+          console.log(error);
+        },
+        complete: () => this.spinner.hide()
+      });
+  }
+
+  voltar() {
+    this.router.navigate(['/professores']);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
+  }
 }
--- SmartSchoolApp/src/app/components/professores/professores.component.ts
+++ SmartSchoolApp/src/app/components/professores/professores.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Util } from '../../util/util';
+import { Disciplina } from '../../models/Disciplina';
+import { Professor } from '../../models/Professor';
+import { Subject, takeUntil } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { ProfessorService } from '../../services/professor.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-professores',
@@ -8,9 +16,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessoresComponent implements OnInit {
 
-  constructor() { }
+  public titulo = 'Professores';
+  public professorSelecionado: Professor;
+  private unsubscriber = new Subject<void>();
+
+  public professores: Professor[];
+
+  constructor(
+    private router: Router,
+    private professorService: ProfessorService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService) {}
+
+  carregarProfessores() {
+    this.spinner.show();
+    this.professorService.getAll()
+      .pipe(takeUntil(this.unsubscriber))
+      .subscribe((professores: Professor[]) => {
+        this.professores = professores;
+        this.toastr.success('Professores foram carregado com Sucesso!');
+      }, (error: any) => {
+        this.toastr.error('Professores não carregados!');
+        console.log(error);
+      }, () => this.spinner.hide()
+    );
+  }
 
   ngOnInit() {
+    this.carregarProfessores();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
+  }
+
+  disciplinaConcat(disciplinas: Disciplina[]) {
+    return Util.nomeConcat(disciplinas);
   }
 
 }
